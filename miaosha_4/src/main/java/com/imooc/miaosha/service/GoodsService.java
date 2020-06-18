6eda47123f2ef73ee32dd837426e93bdd1b35f93
lbybly <1214775720@qq.com>
2020-06-18 17:30:07 +0800

    demiaosha

---
+++ miaosha_4/src/main/java/com/imooc/miaosha/service/GoodsService.java
@@ -0,0 +1,33 @@
+package com.imooc.miaosha.service;
+
+
+
+import com.imooc.miaosha.dao.GoodsDao;
+import com.imooc.miaosha.domain.MiaoshaGoods;
+import com.imooc.miaosha.vo.GoodsVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GoodsService {
+
+    @Autowired
+    GoodsDao goodsDao;
+
+    public List<GoodsVo> listGoodsVo(){
+        return goodsDao.listGoodsVo();
+    }
+
+    public GoodsVo getGoodsVoByGoodsId(long goodsId) {
+        return goodsDao.getGoodsVoByGoodsId(goodsId);
+    }
+
+    public void reduceStock(GoodsVo goodsVo) {
+        MiaoshaGoods miaoshaGoods = new MiaoshaGoods();
+        miaoshaGoods.setGoodsId(goodsVo.getId());
+        goodsDao.reduceStock(miaoshaGoods);     //减库存其实就是根据商品id更新商品库存为原来库存减1，库存减1可以直接放到sql语句中实现
+    }
+}
+
