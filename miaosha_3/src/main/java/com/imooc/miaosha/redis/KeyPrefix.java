798f7cf736e6f046cb1fe3d7bbd5309a7861a431
lbybly <1214775720@qq.com>
2020-06-18 18:10:36 +0800

    miaosha

--- miaosha_3/src/main/java/com/imooc/miaosha/redis/KeyPrefix.java
+++
@@ -1,8 +0,0 @@
-package com.imooc.miaosha.redis;
-
-public interface KeyPrefix {
-        public int expireSeconds();
-
-        public String getPrefix();
-
-}
--- miaosha_4/src/main/java/com/imooc/miaosha/redis/OrderKey.java
+++
@@ -1,8 +0,0 @@
-package com.imooc.miaosha.redis;
-
-public class OrderKey extends BassPrefix {
-
-    public OrderKey(int expireSeconds, String prefix) {
-        super(expireSeconds, prefix);
-    }
-}
